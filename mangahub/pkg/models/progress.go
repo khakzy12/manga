560fb73c3ed2ab4578d0d38da322fb3e678b885f
Khang Dang <84401306+khakzy12@users.noreply.github.com>
2025-12-25 05:33:33 +0700

    Delete mangahub directory

--- mangahub/pkg/models/progress.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// ProgressUpdate must be Capitalized to be exported
-type ProgressUpdate struct {
-	Username string `json:"username"`
-	MangaID  string `json:"manga_id"`
-	Progress string `json:"progress"`
-	Chapter  string `json:"chapter"`
-}
